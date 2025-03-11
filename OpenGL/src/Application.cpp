0254a8a43251e2765871af679067cba163ee04c6
Siddhant <shindesiddhant709@gmail.com>
2025-03-12 01:03:50 +0530

    Rendered a triangle on the screen. Set up to add shaders.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -3,6 +3,40 @@
 
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, std::string source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_LINK_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to load " << (type == GL_FRAGMENT_SHADER ? "fragment" : "vertex") << " shader!" << std::endl;
+        std::cout << message << std::endl;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(std::string vertexShader, std::string fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -29,11 +63,31 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    float positions[6] =
+    {
+        -0.5f,-0.5f,
+        0.5f,-0.5f,
+        0.0f,0.5f
+    };
+
+    unsigned int vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    // TODO : import shader files from Ellie, parse them into strings and pass them to create shaders here
+    // will need to make some sort of shader parsing class
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
